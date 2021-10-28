c8530fc956ae27689da760431e1e8701e22dad33
Mathieu Neveu <mathieu@komeocreation.fr>
2021-10-28 09:45:18 +0200

    socket listener front side

--- front-end/main.js
+++ front-end/main.js
@@ -2,13 +2,20 @@
     const server = 'http://127.0.0.1:3000'
     const socket = io(server);
 
+    //socket part
     socket.on('notification', (data) => {
         console.log('Message depuis le seveur:', data);
     })
 
+    //api calls
     fetch(`${server}/test`).then((res) => {
         return res.json()
     }).then((data) => {
         console.log(data);
     })
 })()
+
+function sendMessage() {
+    socket.emit('messageToAll', { type : 'new_public_message', data : document.querySelector('#messageInput').value})
+    // console.log(document.querySelector('#messageInput').value)
+}
